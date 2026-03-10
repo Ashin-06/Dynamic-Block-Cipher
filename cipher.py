e82f83fc59d1d2c83c3a88c1ca75ef1f0d9f1651
Ashin-06 <ashin1356790@gmail.com>
2026-03-10 10:00:00 +0530

    Implement basic bit permutation layer

--- cipher.py
+++ cipher.py
@@ -1,4 +1,6 @@
-SBOX = [0x63, 0x7c, 0x77] # S-box skeleton
+SBOX = [0x63, 0x7c, 0x77]
+def permute_bits(block_bytes, perm_table):
+    return block_bytes
 class FeistelCipher:
     def __init__(self, password):
         pass
